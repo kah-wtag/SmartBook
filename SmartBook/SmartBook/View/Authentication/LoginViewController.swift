909b794acaa340746d0c967d5bf18fa0ff8a1621
kah-wtag <kamrul.hasan@welldev.io>
2025-07-20 16:39:32 +0600

    A clean UIKit implementation of an Authentication screen using UISegmentedControl and child view controllers to toggle between Login and Signup forms inside a single container view. - Uses a single UIViewController (AuthenticationViewController) with a UISegmentedControl. - Dynamically switches between LoginViewController and SignupViewController. - Follows clean code principles using: 	* Reusable child view controllers 	* Enum for segment type 	* Constants for storyboard identifiers 	* UITextField + UIView + UIButton extensions for styling

---
+++ SmartBook/SmartBook/View/Authentication/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  SmartBook
+//
+//  Created by Md. Kamrul Hasan on 17/7/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var emailLoginTextField: UITextField!
+    @IBOutlet var passwordLoginTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        emailLoginTextField.setStyledPlaceholder("Email")
+        emailLoginTextField.applyUnderline()
+        emailLoginTextField.addPadding([.left, .right], width: 8)
+        passwordLoginTextField.setStyledPlaceholder("Password")
+        passwordLoginTextField.applyUnderline()
+        passwordLoginTextField.addPadding([.left, .right], width: 8)
+        loginButton.applyButtonRoundBorder(borderColor: .blue, borderWidth: 3, cornerRadius: 10)
+    }
+
+    
+}
+
---
+++ SmartBook/SmartBook/View/Authentication/SignupViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignupViewController.swift
+//  SmartBook
+//
+//  Created by Md. Kamrul Hasan on 17/7/25.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+    
+    @IBOutlet var emailSignupTextField: UITextField!
+    @IBOutlet var passwordSignupTextField: UITextField!
+    @IBOutlet var confirmPasswordSignupTextField: UITextField!
+    @IBOutlet var signupButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        emailSignupTextField.setStyledPlaceholder("Email")
+        emailSignupTextField.addPadding([.left, .right], width: 8)
+        emailSignupTextField.applyUnderline()
+        passwordSignupTextField.setStyledPlaceholder("Password")
+        passwordSignupTextField.addPadding([.left, .right], width: 8)
+        passwordSignupTextField.applyUnderline()
+        confirmPasswordSignupTextField.setStyledPlaceholder("Confirm Password")
+        confirmPasswordSignupTextField.addPadding([.left, .right], width: 8)
+        confirmPasswordSignupTextField.applyUnderline()
+        signupButton.applyButtonRoundBorder(borderColor: .blue, borderWidth: 3, cornerRadius: 10)
+
+    }
+    
+}
