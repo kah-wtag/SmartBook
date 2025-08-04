58979efab2ba619ebd8073e0faddb289ef649de8
kah-wtag <kamrul.hasan@welldev.io>
2025-08-04 11:56:00 +0600

    updated user profile view

--- SmartBook/SmartBook/View/Profile/UserProfileViewController.swift
+++ SmartBook/SmartBook/View/Profile/UserProfileViewController.swift
@@ -13,6 +13,7 @@ class UserProfileViewController: UIViewController {
     @IBOutlet var editNumberTextField: UITextField!
     @IBOutlet var editMailTextField: UITextField!
     @IBOutlet var editDesignationTextField: UITextField!
+    @IBOutlet var saveUpdatedProfileButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +22,14 @@ class UserProfileViewController: UIViewController {
     
     private func setupUI() {
         let textFields = [editNameTextField, editNumberTextField, editMailTextField, editDesignationTextField]
-        let placeholders = ["Name", "Phone Number", "Email", "Designation"]
+        let placeholders = ["Md. Kamrul Hasan", "+8801749-140494", "kamrul@gmail.com", "Neurologist"]
         for (field, placeholder) in zip(textFields, placeholders) {
             field?.setStyledPlaceholder(placeholder)
             field?.applyUnderline()
             field?.addPadding([.left, .right], width: 8)
         }
+        
+        saveUpdatedProfileButton.applyButtonRoundBorder(borderColor: .darkGray, borderWidth: 3, cornerRadius: 10)
     }
     
 }
