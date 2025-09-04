0ca7e0a85fa49ca3c1cba2d641cf8331d77438de
kah-wtag <kamrul.hasan@welldev.io>
2025-09-04 14:48:29 +0600

    removed comment

--- SmartBook/SmartBook/Extensions/UIButton+Extension.swift
+++ SmartBook/SmartBook/Extensions/UIButton+Extension.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIButton {
-    func applyButtonRoundBorder(borderColor: UIColor = .buttonBorder, borderWidth: CGFloat = 4, cornerRadius: CGFloat = 8) {
+    func applyButtonRoundBorder(borderColor: UIColor = .border, borderWidth: CGFloat = 4, cornerRadius: CGFloat = 8) {
         layer.borderColor = borderColor.cgColor
         layer.borderWidth = borderWidth
         layer.cornerRadius = cornerRadius
