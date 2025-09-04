83f43facd73077fe9bf2c5995d5e7bcf7e003723
kah-wtag <kamrul.hasan@welldev.io>
2025-09-04 14:48:33 +0600

    Update SignupViewController.swift

--- SmartBook/SmartBook/View/Authentication/SignupViewController.swift
+++ SmartBook/SmartBook/View/Authentication/SignupViewController.swift
@@ -22,13 +22,10 @@ class SignupViewController: UIViewController {
     private func setupUI() {
         signupButton.titleLabel?.font = .textSize(ofSize: .extraLarge, weight: .medium)
         emailSignupTextField.setStyledPlaceholder("Email")
-//        emailSignupTextField.addPadding([.left, .right], width: 8)
         emailSignupTextField.applyUnderline()
         passwordSignupTextField.setStyledPlaceholder("Password")
-//        passwordSignupTextField.addPadding([.left, .right], width: 8)
         passwordSignupTextField.applyUnderline()
         confirmPasswordSignupTextField.setStyledPlaceholder("Confirm Password")
-//        confirmPasswordSignupTextField.addPadding([.left, .right], width: 8)
         confirmPasswordSignupTextField.applyUnderline()
         signupButton.applyButtonRoundBorder(borderColor: .textfield, borderWidth: 3, cornerRadius: 10)
         
