826e85bcf178c4e19ad17b5a91b5bc01d6fabe30
kah-wtag <kamrul.hasan@welldev.io>
2025-09-07 17:11:27 +0600

    custom font and removed duplicate

--- SmartBook/SmartBook/Extensions/UIButton+Extension.swift
+++ SmartBook/SmartBook/Extensions/UIButton+Extension.swift
@@ -8,12 +8,8 @@
 import UIKit
 
 extension UIButton {
-    enum TextSize : Int {
-        case extraLarge = 19, regular = 15, small = 12
-    }
-    
-    func setFontSize(_ size: TextSize, weight: UIFont.Weight = .regular) {
-        self.titleLabel?.font = UIFont.systemFont(ofSize: CGFloat(size.rawValue), weight: weight)
+    func setFontSize(_ size: UIFont.TextSize, weight: UIFont.Weight = .regular) {
+        self.titleLabel?.font = UIFont.textSize(ofSize: size, weight: weight)
     }
     
     func applyButtonRoundBorder(borderColor: UIColor = .border, borderWidth: CGFloat = 4, cornerRadius: CGFloat = 8) {
@@ -23,5 +19,3 @@ extension UIButton {
         layer.masksToBounds = true
     }
 }
-
-
--- SmartBook/SmartBook/Extensions/UIFont+Extension.swift
+++ SmartBook/SmartBook/Extensions/UIFont+Extension.swift
@@ -8,14 +8,15 @@
 import UIKit
 
 extension UIFont {
-    
-    enum TextSize : Int {
+    enum TextSize: Int {
         case extraLarge = 19, regular = 15, small = 12
     }
     
-    static func textSize(ofSize size: TextSize, weight: UIFont.Weight = .regular) -> UIFont {
-        UIFont.systemFont(ofSize: CGFloat(size.rawValue), weight: weight)
+    static func textSize(ofSize size: TextSize, weight: UIFont.Weight = .regular, customFontName: String? = nil) -> UIFont {
+        if let customFont = UIFont(name: "Helvetica Neue", size: CGFloat(size.rawValue)) {
+            return customFont
+        }
+        return UIFont.systemFont(ofSize: CGFloat(size.rawValue), weight: weight)
     }
 }
-
 
