946b36966d5ccaa3a90427a462677a5cb9d413e3
kah-wtag <kamrul.hasan@welldev.io>
2025-09-18 17:25:25 +0600

    Implemented Professionals provile View
    fixed naming and professionals count logic
    
    fixed naming and professionals count logic
    
    Set ServiceProviderVC's Title
    
    removed unused numberOfProfessionals method
    
    Update SmartServiceFieldListCell.swift
    
    Update SmartServiceFieldListCell.swift
    
    removed VC's direct access to viewmodel
    
    removed comment
    
    removed comment
    
    Update ServiceProviderViewModel.swift
    
    removed unnecessary print
    
    removed unnecessary print
    
    PR URL
    
    Update AuthenticationService.swift
    
    no access for vc to data
    
    no access for vc to data
    
    removed weak from iboutlet
    
    changed attribute name
    
    changed attribute name
    
    professionals -> service providers
    
    renamed: fetchProfessionals -> fetchServiceProviders
    
    renamed: fetchProfessionals -> fetchServiceProviders
    
    Update SmartServiceFieldListCell.swift
    
    removed cell duty from vc
    
    whitespace removed
    
    whitespace removed
    
    reduce methods parameter
    
    added final
    
    added final
    
    renamed configureViewModel
    
    renamed configureViewModel
    
    Update ServiceProviderViewController.swift
    
    used higher order function for fetchServiceProviders
    
    Update ServiceProviderViewModel.swift
    
    folder restructured
    
    folder restructured
    
    refactored service and viewmodel class
    
    refactored service and viewmodel class
    
    private serviceProvider
    
    resolved PR review comment
    
    resolved PR review comment
    
    PR URL
    
    used flatmap
    
    if no data, still pull to refresh to fetch data
    
    if no data, still pull to refresh to fetch data
    
    PR review resolved
    
    PR review resolved
    
    pr review resolved
    
    renamed all
    
    remove list name
    
    renamed all
    
    remove list name
    
    Update SmartBookingService.swift
    
    Update SmartBookingService.swift
    
    removed force to refresh
    
    removed force to refresh
    
    Update SmartServiceFieldsViewController.swift
    
    Update SmartServiceFieldsViewController.swift
    
    Update SmartBookingService.swift
    
    remove empty pull to refresh
    
    remove empty pull to refresh
    
    Update SmartServicesViewModel.swift
    
    each screen new load
    
    no loaded error handled
    
    no loaded error handled
    
    implemented authentication service
    
    Update AuthenticationViewModel.swift
    
    used computed var and guard let
    
    used computed var and guard let
    
    removed extra check for refresh control
    
    smartService -> smartServices
    
    smartService -> smartServices
    
    added cell viewmodel
    
    professionalsCountText -> serviceProvidersCount
    
    professionalsCountText -> serviceProvidersCount
    
    removed back navigation text
    
    Update SmartBookingService.swift
    
    Update SmartBookingService.swift
    
    making cell class dumb
    
    removed private from cellviewmodel and cell class instance
    
    removed private from cellviewmodel and cell class instance
    
    bring init up
    
    bring init up
    
    Update ServiceProviderCellViewModel.swift
    
    renamed servicefieldcell -> servicefieldscell
    
    renamed servicefieldcell -> servicefieldscell
    
    Update SmartBookingService.swift
    
    used latest syntex
    
    used latest syntex
    
    extra white space removed
    
    removed configureViewModel method as not following srp
    
    removed private(set) from SmartServicesViewModel
    
    Update SmartServiceFieldsViewModel.swift
    
    private var
    
    added private var
    
    smartservicelist -> smartservices
    
    renamed VC Cell var name in routes
    
    renamed
    
    renamed for clean code
    
    renamed for clean code
    
    folder renamed
    
    Update SmartServiceFieldsViewModel.swift
    
    Update SmartServiceFieldsViewModel.swift
    
    Update SmartServiceFieldsViewController.swift
    
    Update ServiceProviderViewModel.swift
    
    added
    
    Sync with implement_professionals_lists_view
    
    implemented service provider profile view
    
    Update ServiceProviderProfileViewModel.swift
    
    Update SmartBookingService.swift
    
    Update ServiceProviderViewModel.swift
    
    Update ServiceProviderProfileViewController.swift
    
    Update UIButton+Extension.swift
    
    new line in computed variables
    
    separate function for setup text size
    
    Update ServiceProviderProfileViewController.swift
    
    used border color for button border
    
    new line for computed variable
    
    Update ServiceProviderProfileViewModel.swift

---
+++ SmartBook/SmartBook/View/Smart Services/Servie Provider Profile/ServiceProviderProfileViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ServiceProviderProfileViewController.swift
+//  SmartBook
+//
+//  Created by Md. Kamrul Hasan on 19/8/25.
+//
+
+import UIKit
+
+final class ServiceProviderProfileViewController: UIViewController {
+    @IBOutlet var bookAppointmentButton: UIButton!
+    @IBOutlet var mapViewButton: UIButton!
+    @IBOutlet var professionalsImageView: UIImageView!
+    @IBOutlet var professionalsProfileName: UILabel!
+    @IBOutlet var professionalsProfilePatientNumber: UILabel!
+    @IBOutlet var medicalCollege: UILabel!
+    @IBOutlet var professionalsProfileExperience: UILabel!
+    @IBOutlet var professionalsFieldName: UILabel!
+    @IBOutlet var professionalsDegree: UILabel!
+    @IBOutlet var professionalsPersonalBio: UILabel!
+    @IBOutlet var professionalsPersonalBioDetials: UILabel!
+    
+    var viewModel: ServiceProviderProfileViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = viewModel.nameText
+        setupUI()
+        updateUI()
+    }
+    
+    private func setupUI() {
+        setupViewAppearance()
+        setupButtonFontSize()
+        setupLabelFontSize()
+        setupTextColor()
+    }
+    
+    private func setupButtonFontSize() {
+        bookAppointmentButton.setFontSize(.large, weight: .medium, dynamic: true)
+        mapViewButton.setFontSize(.regular, weight: .medium, dynamic: true)
+    }
+    
+    private func setupLabelFontSize() {
+        professionalsProfileName.setFontSize(.large, weight: .bold, dynamic: true)
+        professionalsProfilePatientNumber.setFontSize(.regular, weight: .medium, dynamic: true)
+        professionalsProfileExperience.setFontSize(.regular, weight: .medium, dynamic: true)
+        medicalCollege.setFontSize(.small, weight: .thin, dynamic: true)
+        professionalsPersonalBio.setFontSize(.large, weight: .bold, dynamic: true)
+        professionalsPersonalBioDetials.setFontSize(.small, weight: .thin, dynamic: true)
+        professionalsFieldName.setFontSize(.regular, weight: .medium, dynamic: true)
+        professionalsDegree.setFontSize(.small, weight: .thin, dynamic: true)
+    }
+    
+    private func setupViewAppearance() {
+        professionalsImageView.makeCircular()
+        mapViewButton.applyRoundBorder()
+        bookAppointmentButton.applyRoundBorder()
+    }
+    
+    private func setupTextColor() {
+        bookAppointmentButton.titleLabel?.textColor = .secondaryText
+        mapViewButton.titleLabel?.textColor = .secondaryLabel
+        professionalsProfileName.textColor = .primaryText
+        professionalsProfilePatientNumber.textColor = .primaryText
+        medicalCollege.textColor = .primaryText
+        professionalsProfileExperience.textColor = .primaryText
+        professionalsFieldName.textColor = .primaryText
+        professionalsDegree.textColor = .primaryText
+        professionalsPersonalBio.textColor = .primaryText
+        professionalsPersonalBioDetials.textColor = .primaryText
+    }
+    
+    private func updateUI() {
+        professionalsProfileName.text = viewModel.nameText
+        professionalsProfileExperience.text = viewModel.experienceText
+        professionalsFieldName.text = viewModel.fieldText
+        professionalsImageView.image = viewModel.image
+        professionalsProfilePatientNumber.text = viewModel.patientCountText
+        professionalsDegree.text = viewModel.degreesText
+        medicalCollege.text = viewModel.institutionText
+        professionalsPersonalBioDetials.text = viewModel.bioText
+    }
+}
