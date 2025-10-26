b6ad9c17cc90c489b9868dc28f58df15b4dc6334
kah-wtag <kamrul.hasan@welldev.io>
2025-10-26 09:32:01 +0600

    Update SmartBookingService.swift

--- SmartBook/SmartBook/View/Smart Services/SmartBookingService.swift
+++ SmartBook/SmartBook/View/Smart Services/SmartBookingService.swift
@@ -11,7 +11,7 @@ final class SmartBookingService {
     
     static let shared = SmartBookingService()
     
-    private let baseURL = "https://booking.free.beeceptor.com/data"
+    private let baseURL = "https://bookinggggggg.free.beeceptor.com/data"
     
     private init() { }
     
