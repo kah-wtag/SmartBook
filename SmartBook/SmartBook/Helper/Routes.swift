3eb65cce5b1f54da2574768ca0b81dba26cd8273
kah-wtag <kamrul.hasan@welldev.io>
2025-10-27 09:11:16 +0600

    added serviceprovider loacation storyboard

--- SmartBook/SmartBook/Helper/Routes.swift
+++ SmartBook/SmartBook/Helper/Routes.swift
@@ -18,6 +18,7 @@ struct Routes {
         static let serviceProviderProfile = "Service Provider Profile"
         static let bookingForm = "Booking Form"
         static let appointmentList = "Appointment List"
+        static let serviceProviderLocation = "Service Provider Location"
     }
     
     struct Identifier {
@@ -40,6 +41,7 @@ struct Routes {
         static let upcomingAppointmentVC = "UpcomingAppointmentListViewController"
         static let pastAppointmentVC = "PastAppointmentListViewController"
         static let appointmentCell = "AppointmentListTableViewCell"
+        static let serviceProviderMapVC = "ServiceProviderMapViewController"
     }
 }
 
@@ -122,6 +124,12 @@ extension Routes {
     }
     static var pastAppointmentVC: PastAppointmentListViewController {
         instantiateVC(from: StoryboardName.appointmentList, identifier: Identifier.pastAppointmentVC)
+    }
+}
+
+extension Routes {
+    static var serviceProviderMapVC: ServiceProviderMapViewController {
+        instantiateVC(from: StoryboardName.serviceProviderLocation, identifier: Identifier.serviceProviderMapVC)
     }
 }
 
---
+++ SmartBook/SmartBook/View/Smart Services/Location/ServiceProviderMapViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  ServiceProviderMapViewModel.swift
+//  SmartBook
+//
+//  Created by Md. Kamrul Hasan on 27/10/25.
+//
+
