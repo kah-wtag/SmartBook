0a0e0d6ae4e658e8e592340f6f77cc879fd3a39c
kah-wtag <kamrul.hasan@welldev.io>
2025-10-27 10:08:45 +0600

    cleand code and alert fixed

--- SmartBook/SmartBook/View/Smart Services/SmartBookingService.swift
+++ SmartBook/SmartBook/View/Smart Services/SmartBookingService.swift
@@ -11,8 +11,8 @@ final class SmartBookingAPIService {
     
     static let shared = SmartBookingAPIService()
     
-    private let baseURL = "https://bookinggggggg.free.beeceptor.com/data"
-    private let appointmentURL = "https://bookinggggggg.free.beeceptor.com/data1"
+    private let baseURL = "https://booking.free.beeceptor.com/data"
+    private let appointmentURL = "https://booking.free.beeceptor.com/data1"
     
     private init() { }
     
